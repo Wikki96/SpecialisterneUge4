f22132a3316eed8db26ea89fe7cbf2d260e3cb1f
Viktor Hjort <vfhjort@gmail.com>
2025-03-17 13:25:43 +0100

    CRUD added

--- src/mysql_connector.py
+++ src/mysql_connector.py
@@ -14,21 +14,36 @@ class MySQLConnector:
         self.cursor.execute(query)
         return
     
-    def __commit__(self):
+    def __commit(self):
         self.con.commit()
         return
     
-    def create(self, data):
-        insert = "INSERT INTO orders_combined VALUES " + data 
+    def create(self, row):
+        row = row.split(",")
+        row = ["'" + entry + "'" for entry in row]
+        insert = "INSERT INTO orders_combined VALUES " \
+                 + "(" + ", ".join(row) + ")" 
         self.execute_query(insert)
-        self.__commit__()
+        self.__commit()
         return
     
-    def read(self, columns):
-        select = "SELECT " + "".join(columns) + "FROM orders_combined"
+    def read(self, columns, where=""):
+        if where != "":
+            where = "WHERE " + where
+        select = "SELECT " + ", ".join(columns) + " FROM orders_combined " \
+                    + where
         self.execute_query(select)
         data = self.cursor.fetchall()
         return data
     
-    def update(self, set, where):
+    def update(self, what, where):
+        update = "UPDATE orders_combined SET " + what + " WHERE " + where
+        self.execute_query(update)
+        self.__commit()
+        return
+    
+    def delete(self, where):
+        delete = "DELETE FROM orders_combined WHERE " + where
+        self.execute_query(delete)
+        self.__commit()
         return
--- test/crud_tests.py
+++ test/crud_tests.py
@@ -6,6 +6,11 @@ from src.mysql_connector import MySQLConnector
 if __name__ == "__main__":
     connection_info = ["root", "250303", "primer"]
     connector = MySQLConnector(*connection_info)
+    connector.create("100,2025-03-14T15:24:45+01:00,Wendy Lockman,wendy.lockman@yahoo.com,Headphones,339.31143")
 
-    data = connector.read('*')
+    data = connector.read(("id", "customer_name"), "id = '100'")
+    print(data)
+    connector.update("customer_name = 'Johm Smith'", "id = '100'")
+    data = connector.read(("id", "customer_name"), "id = '100'")
+    connector.delete("id = '100'")
     print(data)
